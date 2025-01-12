1f0fd5b6915322a4057e9c302729c66b90c7c9c4
Dave Conroy <dave@tiredofit.ca>
2025-01-11 21:47:34 -0800

    hardware/backlight - add options for keys.up|down

--- nixos/hardware/backlight.nix
+++ nixos/hardware/backlight.nix
@@ -12,6 +12,20 @@ in
         type = with types; bool;
         description = "Enables tools for display backlight control";
       };
+      keys = {
+        down = {
+          enable = mkOption {
+          default = 224;
+          type = with types; int;
+          description = "Key to increase brightness";
+        };
+        up = {
+          enable = mkOption {
+          default = 225;
+          type = with types; int;
+          description = "Key to increase brightness";
+        };
+      };
     };
   };
 
@@ -20,9 +34,19 @@ in
     services.actkbd = {
       enable = true;
       bindings = [
-        { keys = [ 233 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
-        { keys = [ 232 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+        { keys = [ cfg.keys.up ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
+        { keys = [ cfg.keys.down ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
       ];
     };
+  };  programs.light.enable = true;
+  services.actkbd = {
+    enable = true;
+    bindings = [
+      { keys = [ 224 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -U 10"; }
+      { keys = [ 225 ]; events = [ "key" ]; command = "/run/current-system/sw/bin/light -A 10"; }
+    ];
   };
+
+
+# 232 233 for asus
 }
