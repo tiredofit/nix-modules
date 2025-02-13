2b6e041e2d2d1003624a8cf4f291d0e36bebb507
Dave Conroy <dave@tiredofit.ca>
2025-02-12 19:40:45 -0800

    hardware/bluetooth - add pipewire codec settings

--- nixos/hardware/bluetooth.nix
+++ nixos/hardware/bluetooth.nix
@@ -31,6 +31,15 @@ in
       };
     };
 
+    services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = mkIf (config.host.hardware.sound.enable && config.host.hardware.sound.server == "pipewire") {
+      "monitor.bluez.properties" = {
+          "bluez5.enable-sbc-xq" = true;
+          "bluez5.enable-msbc" = true;
+          "bluez5.enable-hw-volume" = true;
+          "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+      };
+    };
+
     services.blueman.enable = true;
 
     host.filesystem.impermanence.directories = mkIf config.host.filesystem.impermanence.enable [
