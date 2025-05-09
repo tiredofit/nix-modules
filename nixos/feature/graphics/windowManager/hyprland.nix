c23812830a6b8fa1ee29b7e37d3d16b00cc9c69a
Dave Conroy <dave@tiredofit.ca>
2025-05-08 22:38:03 -0700

    wlr

--- nixos/feature/graphics/windowManager/hyprland.nix
+++ nixos/feature/graphics/windowManager/hyprland.nix
@@ -27,6 +27,7 @@ in
       };
       extraPortals = with pkgs; [
         xdg-desktop-portal-gtk
+        xdg-desktop-portal-wlr
         #xdg-desktop-portal-hyprland
         #xdg-desktop-portal-shana
       ];
