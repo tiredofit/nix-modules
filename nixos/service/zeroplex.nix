16f54bccebf178e899a5f92304d61367be60082a
Dave Conroy <dave@tiredofit.ca>
2025-07-08 19:16:54 -0700

    service/zeroplex - update default port

--- nixos/service/zeroplex.nix
+++ nixos/service/zeroplex.nix
@@ -106,7 +106,7 @@ in
             };
             port = mkOption {
               type = types.int;
-              default = cfg.host.network.vpn.zerotier.port;
+              default = config.host.network.vpn.zerotier.port;
               description = "ZeroTier client port number.";
             };
             token_file = mkOption {
