390b404d5e664ea136c60bd15087a2d9344fa5be
Dave Conroy <dave@tiredofit.ca>
2025-07-14 09:10:00 -0700

    feature/graphics/windowManager/hyprland - comment out portal stuff

--- nixos/feature/graphics/windowManager/hyprland.nix
+++ nixos/feature/graphics/windowManager/hyprland.nix
@@ -10,28 +10,28 @@ in
       hyprland = {
         enable = mkDefault true;
         package = pkgs.hyprland;
-        portalPackage = pkgs.xdg-desktop-portal-hyprland;
+        #portalPackage = pkgs.xdg-desktop-portal-hyprland;
         withUWSM  = true;
         #package = inputs.hyprland.packages.${pkgs.system}.hyprland;
       };
     };
 
-    xdg.portal = {
-      enable = true;
-      xdgOpenUsePortal = true;
-      config.common = {
-        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
-        "org.freedesktop.impl.portal.ScreenCast" = "hyprland";
-        #"org.freedesktop.impl.portal.ScreenCast" = [ "wlr" ];
-        #"org.freedesktop.impl.portal.Screenshot" = [ "wlr" ];
-        #"org.freedesktop.portal.FileChooser" = [ "xdg-desktop-portal-gtk" ];
-      };
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-gtk
-        xdg-desktop-portal-wlr
-        xdg-desktop-portal-hyprland
-        #xdg-desktop-portal-shana
-      ];
-    };
+    #xdg.portal = {
+    #  enable = true;
+    #  xdgOpenUsePortal = true;
+    #  config.common = {
+    #    "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+    #    "org.freedesktop.impl.portal.ScreenCast" = "hyprland";
+    #    #"org.freedesktop.impl.portal.ScreenCast" = [ "wlr" ];
+    #    #"org.freedesktop.impl.portal.Screenshot" = [ "wlr" ];
+    #    #"org.freedesktop.portal.FileChooser" = [ "xdg-desktop-portal-gtk" ];
+    #  };
+    #  extraPortals = with pkgs; [
+    #    xdg-desktop-portal-gtk
+    #    xdg-desktop-portal-wlr
+    #    xdg-desktop-portal-hyprland
+    #    #xdg-desktop-portal-shana
+    #  ];
+    #};
   };
 }
