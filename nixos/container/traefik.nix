a25c3fb8bd4cf7059764ebbd2990ebecc277ef67
Dave Conroy <dave@tiredofit.ca>
2025-08-04 10:42:06 -0700

    container/traefik* - update LETSENCRYPT_DNS_RESOLVER

--- nixos/container/traefik.nix
+++ nixos/container/traefik.nix
@@ -242,6 +242,7 @@ in
         "TRAEFIK_USER" = mkDefault "traefik";
         "LETSENCRYPT_CHALLENGE" = mkDefault "DNS";
         "LETSENCRYPT_DNS_PROVIDER" = mkDefault "cloudflare";
+        "LETSENCRYPT_DNS_RESOLVER" = "1.1.1.1:53,1.0.0.1:53";
         "DOCKER_CONSTRAINTS" = cfg.docker.constraint;
         "DASHBOARD_HOSTNAME" = mkDefault "${hostname}.vpn.${config.host.network.domainname}";
       };
