6508a63ba918941adabcb4640c3a63419dc69648
Dave Conroy <dave@tiredofit.ca>
2025-08-13 08:42:33 -0700

    network/manager - put some failsafes in network manager

--- nixos/network/manager.nix
+++ nixos/network/manager.nix
@@ -13,12 +13,6 @@ in
   };
 
   config = {
-    networking = {
-      networkmanager = mkIf (cfg == "networkmanager") {
-        enable = true;
-      };
-    };
-
     host.filesystem.impermanence.directories = mkIf (config.host.filesystem.impermanence.enable) (
       [
         # Always include these directories if impermanence is enabled
@@ -29,10 +23,28 @@ in
       ] else [])
     );
 
+    networking = {
+      networkmanager = mkIf (cfg == "networkmanager") {
+        enable = true;
+      };
+    };
+
     services = {
       resolved = {
         enable = mkDefault true;
       };
     };
+
+    # https://github.com/systemd/systemd/blob/e1b45a756f71deac8c1aa9a008bd0dab47f64777/NEWS#L13
+    systemd.services.NetworkManager-wait-online.enable = false;
+    systemd.network.wait-online.enable = false;
+
+    # Do not take down the network for too long when upgrading,
+    # This also prevents failures of services that are restarted instead of stopped.
+    # It will use `systemctl restart` rather than stopping it with `systemctl stop`
+    # followed by a delayed `systemctl start`.
+    systemd.services.systemd-networkd.stopIfChanged = false;
+    # Services that are only restarted might be not able to resolve when resolved is stopped before
+    systemd.services.systemd-resolved.stopIfChanged = false;
   };
 }
