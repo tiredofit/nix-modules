38a16f53a25a5f8279896a094bc8163bb48fc248
Dave Conroy <dave@tiredofit.ca>
2025-09-15 10:09:28 -0700

    applications/bash - update bash settings

--- nixos/application/bash.nix
+++ nixos/application/bash.nix
@@ -2,15 +2,23 @@
 let
   cfg = config.host.application.bash;
   shellAliases = {
-    ".." = "cd .." ;
-    "..." = "cd ..." ;
-    home = "cd ~" ;
-    fuck = "sudo $(history -p !!)" ;                                    # run last command as root
-    mkdir = "mkdir -p" ;                                                # no error, create parents
-    scstart = "systemctl start $@";                                     # systemd service start
-    scstop = "systemctl stop $@";                                       # systemd service stop
-    scenable = "systemctl disable $@";                                  # systemd service enable
-    scdisable = "systemctl disable $@";                                 # systemd service disable
+    ".." = "cd ..";
+    "..." = "cd ...";
+    fuck = "sudo $(history -p !!)"; # run last command as root
+    home = "cd ~";
+    mkdir = "mkdir -p";
+    s = "sudo systemctl";
+    scdisable = "sudo systemctl disable $@";
+    scenable = "sudo systemctl  disable $@";
+    scstart = "sudo systemctl start $@";
+    scstop = "sudo systemctl stop $@";
+    sj = "sudo journalctl";
+    u = "systemctl --user";
+    uj = "journalctl --user";
+    uscdisable = "systemctl --user disable $@";
+    uscenable = "systemctl --user disable $@";
+    uscstart = "systemctl --user start $@";
+    uscstop = "systemctl --user stop $@";
   };
 in
   with lib;
@@ -35,121 +43,167 @@ in
         completion.enable = true;
         inherit shellAliases;
         shellInit = ''
-              ## History
-              export HISTFILE=/$HOME/.bash_history
-              ## Configure bash to append (rather than overwrite history)
-              shopt -s histappend
+          ## History
+          export HISTFILE=/$HOME/.bash_history
+          shopt -s histappend
+          shopt -s cmdhist
+          export PROMPT_COMMAND="''${PROMPT_COMMAND:+$PROMPT_COMMAND$"\n"}history -a; history -c; history -r"
+          HISTTIMEFORMAT="%Y%m%d.%H%M%S%z "
+          HISTFILESIZE=2000000
+          HISTSIZE=3000
+          export HISTIGNORE="ls:ll:ls -alh:pwd:clear:history:ps"
+          HISTCONTROL=ignoreboth
 
-              # Attempt to save all lines of a multiple-line command in the same entry
-              shopt -s cmdhist
+          if [ -d "/var/local/data" ] ; then
+            alias vld='cd /var/local/data'
+          fi
 
-              ## After each command, append to the history file and reread it
-              export PROMPT_COMMAND="''${PROMPT_COMMAND:+$PROMPT_COMMAND$"\n"}history -a; history -c; history -r"
+          if [ -d "/var/local/db" ] ; then
+            alias vldb='cd /var/local/db'
+          fi
 
-              ## Print the timestamp of each command
-              HISTTIMEFORMAT="%Y%m%d.%H%M%S%z "
+          if [ -d "/var/local/data/_system" ] ; then
+            alias vlds='cd /var/local/data/_system'
+          fi
 
-              ## Set History File Size
-              HISTFILESIZE=2000000
+          if command -v "nmcli" &>/dev/null; then
+            alias wifi_scan="nmcli device wifi rescan && nmcli device wifi list"  # rescan for network
+          fi
 
-              ## Set History Size in memory
-              HISTSIZE=3000
+          if command -v "curl" &>/dev/null; then
+            alias derp="curl https://cht.sh/$1"                       # short and sweet command lookup
+          fi
 
-              ## Don't save ls,ps, history commands
-              export HISTIGNORE="ls:ll:ls -alh:pwd:clear:history:ps"
+          if command -v "grep" &>/dev/null; then
+            alias grep="grep --color=auto"                            # Colorize grep
+          fi
 
-              ## Do not store a duplicate of the last entered command and any commands prefixed with a space
-              HISTCONTROL=ignoreboth
+          if command -v "netstat" &>/dev/null; then
+              alias ports="netstat -tulanp"                             # Show Open Ports
+          fi
 
-              if [ -d "/var/local/data" ] ; then
-                  alias vld='cd /var/local/data'
+          if command -v "tree" &>/dev/null; then
+            alias tree="tree -Cs"
+          fi
+
+          if command -v "rsync" &>/dev/null; then
+              alias rsync="rsync -aXxtv"                                # Better copying with Rsync
+          fi
+
+          if command -v "rg" &>/dev/null && command -v "fzf" &>/dev/null && command -v "bat" &>/dev/null; then
+            function frg {
+              result=$(rg --ignore-case --color=always --line-number --no-heading "$@" |
+              fzf --ansi \
+                  --color 'hl:-1:underline,hl+:-1:underline:reverse' \
+                  --delimiter ':' \
+                  --preview "bat --color=always {1} --theme='Solarized (light)' --highlight-line {2}" \
+                  --preview-window 'up,60%,border-bottom,+{2}+3/3,~3')
+              file="''${result%%:*}"
+              linenumber=$(echo "''${result}" | cut -d: -f2)
+              if [ ! -z "$file" ]; then
+                $EDITOR +"''${linenumber}" "$file"
               fi
+            }
+          fi
 
-              if [ -d "/var/local/db" ] ; then
-                  alias vldb='cd /var/local/db'
-              fi
+          # systemctl helpers
+          if command -v "fzf" &>/dev/null; then
+            _sysls() {
+                # $1: --system or --user
+                # $2: states, see also "systemctl list-units --state=help"
+                WIDE=$1
+                [ -n $2 ] && STATE="--state=$2"
+                cat \
+                    <(echo 'UNIT/FILE LOAD/STATE ACTIVE/PRESET SUB DESCRIPTION') \
+                    <(systemctl $WIDE list-units --legend=false $STATE) \
+                    <(systemctl $WIDE list-unit-files --legend=false $STATE) \
+                | sed 's/●/ /' \
+                | grep . \
+                | column --table --table-columns-limit=5 \
+                | fzf --header-lines=1 \
+                      --accept-nth=1 \
+                      --no-hscroll \
+                      --preview="SYSTEMD_COLORS=1 systemctl $WIDE status {1}" \
+                      --preview-window=down
+            }
 
-              if [ -d "/var/local/data/_system" ] ; then
-                  alias vlds='cd /var/local/data/_system'
-              fi
+            alias sls='_sysls --system'
+            alias uls='_sysls --user'
+            alias sjf='sj --unit $(uls) --all --follow'
+            alias ujf='uj --unit $(uls) --all --follow'
 
-              if command -v "nmcli" &>/dev/null; then
-                  alias wifi_scan="nmcli device wifi rescan && nmcli device wifi list"  # rescan for network
-              fi
+            _SYS_ALIASES=(
+              sstart sstop sre
+              ustart ustop ure
+            )
 
-              if command -v "curl" &>/dev/null; then
-                  alias derp="curl https://cht.sh/$1"                       # short and sweet command lookup
-              fi
+            _SYS_CMDS=(
+              's start $(sls static,disabled,failed)'
+              's stop $(sls running,failed)'
+              's restart $(sls)'
+              'u start $(uls static,disabled,failed)'
+              'u stop $(uls running,failed)'
+              'u restart $(uls)'
+            )
 
-              if command -v "grep" &>/dev/null; then
-                  alias grep="grep --color=auto"                            # Colorize grep
-              fi
-
-              if command -v "netstat" &>/dev/null; then
-                  alias ports="netstat -tulanp"                             # Show Open Ports
-              fi
-
-              if command -v "tree" &>/dev/null; then
-                  alias tree="tree -Cs"
-              fi
-
-              if command -v "rsync" &>/dev/null; then
-                  alias rsync="rsync -aXxtv"                                # Better copying with Rsync
-              fi
-
-              if command -v "rg" &>/dev/null && command -v "fzf" &>/dev/null && command -v "bat" &>/dev/null; then
-                function frg {
-                  result=$(rg --ignore-case --color=always --line-number --no-heading "$@" |
-                    fzf --ansi \
-                        --color 'hl:-1:underline,hl+:-1:underline:reverse' \
-                        --delimiter ':' \
-                        --preview "bat --color=always {1} --theme='Solarized (light)' --highlight-line {2}" \
-                        --preview-window 'up,60%,border-bottom,+{2}+3/3,~3')
-                  file="''${result%%:*}"
-                  linenumber=$(echo "''${result}" | cut -d: -f2)
-                  if [ ! -z "$file" ]; then
-                          $EDITOR +"''${linenumber}" "$file"
-                  fi
-                }
-              fi
-
-              if [ -d "$HOME/.bashrc.d" ] ; then
-                for script in $HOME/.bashrc.d/* ; do
-                    source $script
+            _sysexec() {
+                for ((j=0; j < ''${#_SYS_ALIASES[@]}; j++)); do
+                    if [ "$1" == "''${_SYS_ALIASES[$j]}" ]; then
+                      cmd=$(eval echo "''${_SYS_CMDS[$j]}") # expand service name
+                      wide=''${cmd:0:1}
+                      cmd="$cmd && ''${wide} status \$_ || ''${wide}j -xeu \$_"
+                      eval $cmd
+                      [ -n "$BASH_VERSION" ] && history -s $cmd
+                      #[ -n "$ZSH_VERSION" ] && print -s $cmd
+                      return
+                    fi
                 done
+            }
+
+            for i in ''${_SYS_ALIASES[@]}; do
+              source /dev/stdin <<EOF
+            $i() {
+              _sysexec $i
+            }
+EOF
+            done
+
+          fi
+
+          if [ -d "$HOME/.bashrc.d" ] ; then
+            for script in $HOME/.bashrc.d/* ; do
+                source $script
+            done
+          fi
+          sir() {
+               if [ -z $1 ] || [ -z $2 ] ; then echo "Search inside Replace: sir <find_string_named> <sring_replaced>" ; return 1 ; fi
+               for file in $(rg -l $1) ; do
+                    sed -i "s|$1|$2|g" "$file"
+               done
+          }
+
+          far() {
+            if [ -z $1 ] || [ -z $2 ] ; then echo "Rename files: far <find_file_named> <file_renamed>" ; return 1 ; fi
+            for file in $(find -name "$1") ; do
+                 mv "$file" $(dirname "$file")/$2
+            done
+          }
+
+          # Quickly run a pkg run nixpkgs - Add a second argument to it otherwise it will simply run the command
+          pkgrun () {
+            if [ -n $1 ] ; then
+              local pkg
+              pkg=$1
+              if [ "$2" != "" ] ; then
+                shift
+                local args
+                args="$@"
+              else
+                args=$pkg
               fi
-
-              sir() {
-                   if [ -z $1 ] || [ -z $2 ] ; then echo "Search inside Replace: sir <find_string_named> <sring_replaced>" ; return 1 ; fi
-                   for file in $(rg -l $1) ; do
-                        sed -i "s|$1|$2|g" "$file"
-                   done
-              }
-
-              far() {
-                   if [ -z $1 ] || [ -z $2 ] ; then echo "Rename files: far <find_file_named> <file_renamed>" ; return 1 ; fi
-                   for file in $(find -name "$1") ; do
-                        mv "$file" $(dirname "$file")/$2
-                   done
-              }
-
-              # Quickly run a pkg run nixpkgs - Add a second argument to it otherwise it will simply run the command
-              pkgrun () {
-                  if [ -n $1 ] ; then
-                     local pkg
-                     pkg=$1
-                     if [ "$2" != "" ] ; then
-                         shift
-                         local args
-
-                         args="$@"
-                     else
-                         args=$pkg
-                     fi
-
-                     nix-shell -p $pkg.out --run "$args"
-                  fi
-              }
+              nix-shell -p $pkg.out --run "$args"
+            fi
+          }
 
           resetcow() {
             process_path() {
