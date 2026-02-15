5a07bfe7bc13d45dc1f2f2bd1bc50d5c49d5a7de
Dave Conroy <dave@tiredofit.ca>
2026-02-15 10:09:09 -0800

    feature/graphics/displayManager/greetd - set some wayland session options

--- nixos/hardware/keyboard.nix
+++ nixos/hardware/keyboard.nix
@@ -16,7 +16,7 @@ in
   };
 
   config = mkIf cfg.enable {
-    hardware.keyboard.qmk.enable = mkDefault true;
+    hardware.keyboard.qmk.enable = true;
 
     environment.systemPackages = with pkgs; [
       qmk
