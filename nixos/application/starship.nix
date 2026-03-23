5029055baa7ee2c71e4fdce38df80f7a9f20b5b9
Dave Conroy <dave@tiredofit.ca>
2026-03-22 18:06:48 -0700

    applications/starship - init

---
+++ nixos/application/starship.nix
@@ -0,0 +1,64 @@
+{config, lib, pkgs, ...}:
+
+let
+  cfg = config.host.application.starship;
+  tomlFormat = pkgs.formats.toml { };
+  # Only generate a TOML and install it if `programs.starship.settings` is a non-empty attrset.
+  starshipConfig = "/etc/starship.toml";
+  programsSet = config.programs or {};
+  starshipProgram = programsSet.starship or {};
+  settingsAttr = starshipProgram.settings or {};
+  hasSettings = lib.isAttrs settingsAttr && (builtins.length (lib.attrNames settingsAttr) > 0);
+  generatedToml = if hasSettings then tomlFormat.generate "starship.toml" settingsAttr else null;
+in
+  with lib;
+{
+  options = {
+    host.application.starship = {
+      enable = mkOption {
+        default = true;
+        type = with types; bool;
+        description = "Enables starship";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    # Only export STARSHIP_CONFIG when we actually generate a TOML
+    environment = lib.optionalAttrs hasSettings {
+      sessionVariables = {
+        STARSHIP_CONFIG = starshipConfig;
+      };
+      variables = {
+        STARSHIP_CONFIG = starshipConfig;
+      };
+    };
+
+    programs = {
+      bash = {
+        #interactiveShellInit = ''
+        #  if [[ $TERM != "dumb" ]]; then
+        #    eval "$(${pkgs.starship}/bin/starship init bash --print-full-init)"
+        #  fi
+        #'';
+      };
+      zsh = {
+        #interactiveShellInit = ''
+        #  if [[ $TERM != "dumb" ]]; then
+        #    eval "$(${pkgs.starship}/bin/starship init zsh)"
+        #  fi
+        #'';
+      };
+      starship = {
+        enable = true;
+      };
+    };
+
+    # Only add the activation script if we have settings to write
+    system.activationScripts = lib.optionalAttrs hasSettings {
+      starshipToml = lib.stringAfter [ "etc" ] ''
+        install -m 0644 ${generatedToml} ${starshipConfig}
+      '';
+    };
+  };
+}
