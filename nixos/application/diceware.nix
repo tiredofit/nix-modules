a561bae9fd908b7d624671b0c9fc09453a3bbab4
Dave Conroy <dave@tiredofit.ca>
2026-04-10 10:02:18 -0700

    modules/applications/cli/diceware - fix some bash only conversions

--- nixos/application/diceware.nix
+++ nixos/application/diceware.nix
@@ -26,21 +26,21 @@ in
       if [ -n "$1" ] ; then pwords=$1 ; else pwords=4 ; fi
       counter=1
       for s_password in $(eval echo "{1..$pwords}") ; do
-      case $counter in
-              "2" | "4" | "6" | "8" | "10" | "12" | "14" | "16" )
-                      s_password=$(diceware -n 1)
-                      s_password=''${s_password^^}
-              ;;
-              "1" | "3" | "5" | "7" | "9" | "11" | "13" | "15" )
-                      s_password=$(diceware -n 1 --no-caps)
-                      s_password=''${s_password,,}
-              ;;
-      esac
-      if [ "$counter" -lt $pwords ] ; then
-        s_password="$s_password-"
-      fi
-      genpassword=$genpassword$s_password
-      (( counter+=1 ))
+        case $counter in
+          "2" | "4" | "6" | "8" | "10" | "12" | "14" | "16" )
+            s_password=$(diceware -n 1)
+            s_password=$(printf "%s" "$s_password" | tr '[:lower:]' '[:upper:]')
+          ;;
+          "1" | "3" | "5" | "7" | "9" | "11" | "13" | "15" )
+            s_password=$(diceware -n 1 --no-caps)
+            s_password=$(printf "%s" "$s_password" | tr '[:upper:]' '[:lower:]')
+            ;;
+        esac
+        if [ "$counter" -lt $pwords ] ; then
+          s_password="$s_password-"
+        fi
+        genpassword=$genpassword$s_password
+        (( counter+=1 ))
       done
       echo $genpassword
     }
