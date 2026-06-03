3fcebfbfe2eee46000b3624d672303ddaeb856af
Dave Conroy <dave@tiredofit.ca>
2026-06-02 23:20:48 -0700

    feature/graphics/displayManager/gdm - convert to 26.05 syntax

--- nixos/feature/graphics/displayManager/gdm.nix
+++ nixos/feature/graphics/displayManager/gdm.nix
@@ -2,23 +2,10 @@
 with lib;
 let
   graphics = config.host.feature.graphics;
-  wayland =
-    if (graphics.backend == "wayland")
-    then true
-    else false;
 in
 
 {
   config = mkIf (graphics.enable && graphics.displayManager.manager == "gdm") {
-    services = {
-      xserver = {
-        displayManager = {
-          gdm = {
-            enable = mkDefault true;
-            wayland = mkDefault wayland;
-          };
-        };
-      };
-    };
+    services.displayManager.gdm.enable = mkDefault true;
   };
 }
