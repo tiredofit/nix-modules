3e0707458c3e64d9b1b19f75de761ee4e36f8cb4
Dave Conroy <dave@tiredofit.ca>
2026-06-11 17:05:21 -0700

    application/gdu - init

---
+++ nixos/application/gdu.nix
@@ -0,0 +1,23 @@
+{config, lib, pkgs, ...}:
+
+let
+  cfg = config.host.application.gdu;
+in
+  with lib;
+{
+  options = {
+    host.application.gdu = {
+      enable = mkOption {
+        default = false;
+        type = with types; bool;
+        description = "Enables graphical disk usage";
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      gdu
+    ];
+  };
+}
