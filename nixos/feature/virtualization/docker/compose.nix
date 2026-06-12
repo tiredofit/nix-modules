50f03880decd3781a52ecc53b77844d6c479c15c
Dave Conroy <dave@tiredofit.ca>
2026-06-11 17:03:47 -0700

    feature/virtualization/docker/compose - use pkg

--- nixos/feature/virtualization/docker/compose.nix
+++ nixos/feature/virtualization/docker/compose.nix
@@ -15,51 +15,58 @@ in
     };
   };
 
-  config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      unstable.docker-compose
-    ];
+  config = mkIf cfg.enable (
+    let
+      sharedInit = ''
+        docker_compose_bin="${pkgs.unstable.docker-compose}/bin/docker-compose"
+        export DOCKER_COMPOSE_TIMEOUT=''${DOCKER_TIMEOUT:-"120"}
 
-    programs = {
-      bash = {
-        interactiveShellInit = ''
-          docker_compose_bin="$(command -v docker-compose)"
-          export DOCKER_COMPOSE_TIMEOUT=''${DOCKER_TIMEOUT:-"120"}
+        docker-compose() {
+         if [ "$2" != "--help" ] ; then
+           case "$1" in
+             "down" )
+               arg=$(echo "$@" | ${pkgs.gnused}/bin/sed "s|^$1||g")
+               $dsudo $docker_compose_bin down --timeout $DOCKER_COMPOSE_TIMEOUT $arg
+             ;;
+             "restart" )
+               arg=$(echo "$@" | ${pkgs.gnused}/bin/sed "s|^$1||g")
+               $dsudo $docker_compose_bin restart --timeout $DOCKER_COMPOSE_TIMEOUT $arg
+             ;;
+             "stop" )
+               arg=$(echo "$@" | ${pkgs.gnused}/bin/sed "s|^$1||g")
+               $dsudo $docker_compose_bin stop --timeout $DOCKER_COMPOSE_TIMEOUT $arg
+             ;;
+             "up" )
+               arg=$(echo "$@" | ${pkgs.gnused}/bin/sed "s|^$1||g")
+               $dsudo $docker_compose_bin up $arg
+             ;;
+             * )
+               $dsudo $docker_compose_bin ''${@}
+             ;;
+          esac
+         fi
+        }
 
-          docker-compose() {
-           if [ "$2" != "--help" ] ; then
-             case "$1" in
-               "down" )
-                 arg=$(echo "$@" | ${pkgs.gnused}/bin/sed "s|^$1||g")
-                 $dsudo $docker_compose_bin down --timeout $DOCKER_COMPOSE_TIMEOUT $arg
-               ;;
-               "restart" )
-                 arg=$(echo "$@" | ${pkgs.gnused}/bin/sed "s|^$1||g")
-                 $dsudo $docker_compose_bin restart --timeout $DOCKER_COMPOSE_TIMEOUT $arg
-               ;;
-               "stop" )
-                 arg=$(echo "$@" | ${pkgs.gnused}/bin/sed "s|^$1||g")
-                 $dsudo $docker_compose_bin stop --timeout $DOCKER_COMPOSE_TIMEOUT $arg
-               ;;
-               "up" )
-                 arg=$(echo "$@" | ${pkgs.gnused}/bin/sed "s|^$1||g")
-                 $dsudo $docker_compose_bin up $arg
-               ;;
-               * )
-                 $dsudo $docker_compose_bin ''${@}
-               ;;
-            esac
-           fi
-          }
+        alias dcpull='$dsudo docker-compose pull'                                                                                        # Docker Compose Pull
+        alias dcu='$dsudo $docker_compose_bin up'                                                                                   # Docker Compose Up
+        alias dcud='$dsudo $docker_compose_bin up -d'                                                                               # Docker Compose Daemonize
+        alias dcd='$dsudo $docker_compose_bin down --timeout $DOCKER_COMPOSE_TIMEOUT'                                               # Docker Compose Down
+        alias dcl='$dsudo $docker_compose_bin logs -f'                                                                              # Docker Compose Logs
+        alias dcrecycle='$dsudo $docker_compose_bin down --timeout $DOCKER_COMPOSE_TIMEOUT ; $dsudo $docker_compose_bin up -d' # Docker Compose Restart
+      '';
+    in {
+      environment.systemPackages = with pkgs; [
+        unstable.docker-compose
+      ];
 
-          alias dcpull='$dsudo docker-compose pull'                                                                                        # Docker Compose Pull
-          alias dcu='$dsudo $docker_compose_bin up'                                                                                   # Docker Compose Up
-          alias dcud='$dsudo $docker_compose_bin up -d'                                                                               # Docker Compose Daemonize
-          alias dcd='$dsudo $docker_compose_bin down --timeout $DOCKER_COMPOSE_TIMEOUT'                                               # Docker Compose Down
-          alias dcl='$dsudo $docker_compose_bin logs -f'                                                                              # Docker Compose Logs
-          alias dcrecycle='$dsudo $docker_compose_bin down --timeout $DOCKER_COMPOSE_TIMEOUT ; $dsudo $docker_compose_bin up -d' # Docker Compose Restart
-        '';
+      programs = {
+        bash = {
+          interactiveShellInit = sharedInit;
+        };
+        zsh = {
+          interactiveShellInit = sharedInit;
+        };
       };
-    };
-  };
+    }
+  );
 }
